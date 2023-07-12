ed792d89363ff186060acd8db333a541f921ea2b
duvanpalomino <duvanpalomino@gmail.com>
2023-07-12 18:23:24 -0500

    Añadiendo la ventana de Sing Up

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Layout from '../containers/Layout';
 import Login from '../containers/Login';
 import RecoveryPassword from '../containers/RecoveryPassword';
+import SingUp from '../containers/SingUp';
 import Home from '../pages/Home';
 import NotFound from '../pages/NotFound';
 import '../styles/global.css';
@@ -15,6 +16,7 @@ const App = () => {
                     <Route exact path='/' Component={Home} />
                     <Route exact path='/login' Component={Login} />
                     <Route exact path='/recovery-password' Component={RecoveryPassword} />
+                    <Route exact path='sing-up' Component={SingUp} />;
                     <Route exact path='*' Component={NotFound} />
                 </Routes>
             </Layout> 
