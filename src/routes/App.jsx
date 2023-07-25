ac40242f800013aeef192475d6e60979c899ac20
duvanpalomino <duvanpalomino@gmail.com>
2023-07-24 19:47:26 -0500

    Estructuracion del reto

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,30 +1,37 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Layout from '../containers/Layout';
-import Login from '../containers/Login';
-import RecoveryPassword from '../containers/RecoveryPassword';
-import SingUp from '../containers/SingUp';
-import MyAccount from '../containers/MyAccount';
-import MyAccountEdit from '../containers/MyAccountEdit';
 import Home from '../pages/Home';
+import Login from '../pages/Login';
+import PasswordRecovery from '../pages/PasswordRecovery';
+import SendEmail from '../pages/SendEmail';
+import NewPassword from '../pages/NewPassword';
+import MyAccount from '../pages/MyAccount';
+import CreateAccount from '../pages/CreateAccount';
+import Checkout from '../pages/Checkout';
+import Orders from '../pages/Orders';
 import NotFound from '../pages/NotFound';
 import '../styles/global.css';
 
 const App = () => {
-    return (
-        <BrowserRouter>
-            <Layout>
-                <Routes>
-                    <Route exact path='/' Component={Home} />
-                    <Route exact path='/login' Component={Login} />
-                    <Route exact path='/recovery-password' Component={RecoveryPassword} />
-                    <Route exact path='sing-up' Component={SingUp} />
-                    <Route exact path='my-account' Component={MyAccount} />
-                    <Route exact path='my-account-edit' Component={MyAccountEdit} />
-                    <Route exact path='*' Component={NotFound} />
-                </Routes>
-            </Layout> 
-        </BrowserRouter>
-    );
-};
-export default App;  
+	return (
+		<BrowserRouter>
+			<Layout>
+				<Switch>
+					<Route exact path="/" component={Home} />
+					<Route exact path="/login" component={Login} />
+					<Route exact path="/password-recovery" component={PasswordRecovery} />
+					<Route exact path="/send-email" component={SendEmail} />
+					<Route exact path="/new-password" component={NewPassword} />
+					<Route exact path="/account" component={MyAccount} />
+					<Route exact path="/signup" component={CreateAccount} />
+					<Route exact path="/checkout" component={Checkout} />
+					<Route exact path="/orders" component={Orders} />
+					<Route path="*" component={NotFound} />
+				</Switch>
+			</Layout>
+		</BrowserRouter>
+	);
+}
+
+export default App;
