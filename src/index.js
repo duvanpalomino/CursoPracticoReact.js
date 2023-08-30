4097866408ecedb422bd792f95601f6991691bb8
duvanpalomino <duvanpalomino@gmail.com>
2023-08-30 09:45:37 -0500

    imagenes y alias en webpack

--- src/index.js
+++ src/index.js
@@ -1,5 +1,72 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM, { render } from 'react-dom';
 import App from './routes/App';
 
 ReactDOM.render(<App />, document.getElementById('app'));
+
+// Clase de Componentes en React js
+
+// Estructura de Componente StateFull
+
+// import React, { useState } from 'react';
+
+// const Button = () => {
+//     const [name, setName] = useState('Hola Mundo')
+//     return(
+//         <div>
+//              <h1>{name}</h1>
+//         </div>
+//     )
+// }
+
+// Estructura de Componente Stateless
+
+// import React from 'react';
+
+// const Button = () => ( 
+//      <div>
+//          <h1>{name}</h1>
+//      </div>
+// );
+
+// import React from 'react';
+
+// const Button = ( { text } ) => <ButtonRed text = {text} />;
+
+// import React from 'react';
+
+// const Button = () => <ButtonRed />;
+
+// otros ejemplos de componentes con clases en react
+
+// import React, { Component } from 'react';
+
+// class App extends Component {
+
+// constructor(){
+
+// }
+
+// render(){
+//      return (){
+//          <div>
+//              <h1>Hello, World</h1>
+//          </div>
+//          }
+//      } 
+// }
+
+
+// otros tipos de componentes tipo HOC
+
+// import React from 'react';
+
+// function ComponentWrapper(){
+//      class Wrapper extends Component {
+//          render(){
+//              return <WrapperComponent { ...this.props } />;
+//           }
+//      }
+
+//  return  Wrapper;
+// }
--- webpack.config.js
+++ webpack.config.js
@@ -12,10 +12,18 @@ module.exports = {
     },
     resolve: {
         extensions: ['.js', '.jsx'],
+        alias: {
+            '@components': path.resolve(__dirname, 'src/components/'),
+            '@containers': path.resolve(__dirname, 'src/containers/'),
+            '@pages': path.resolve(__dirname, 'src/pages/'),
+            '@styles': path.resolve(__dirname, 'src/styles/'),
+            '@icons': path.resolve(__dirname, 'src/assets/icons/'),
+            '@logos': path.resolve(__dirname, 'src/assets/logos/'),
+        }
     },
     mode: 'development',
     module: {
-         rules: [
+        rules: [
             {
                 test: /\.(js|jsx)$/,
                 exclude: /node_modules/,
@@ -38,8 +46,12 @@ module.exports = {
                     "css-loader",
                     "sass-loader",
                 ]
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                type: 'asset'
             }
-         ]
+        ]
     },
     plugins: [
         new HtmlWebpackPlugin({
